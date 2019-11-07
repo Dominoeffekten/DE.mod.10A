dde800495d3e6dd0eb6d3bde5cef1fd87fc98e94
dominoeffekten <rikkedomino@gmail.com>
2019-11-07 18:10:50 +0100

    update

--- modules/shape.js
+++ modules/shape.js
@@ -1,5 +1,7 @@
 'use strict';
 
+response.setHeader("Access-Control-Allow-Origin", request.getHeader("*"));
+
 const $ = function (foo) {
     return document.getElementById(foo);    // save keystrokes
 }
