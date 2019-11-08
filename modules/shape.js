ec11c9e121bbf7a82fcdfac9e3649009a71dbab9
dominoeffekten <rikkedomino@gmail.com>
2019-11-08 09:56:48 +0100

    Update 1.10

--- modules/shape.js
+++ modules/shape.js
@@ -4,8 +4,6 @@ const $ = function (foo) {
     return document.getElementById(foo);    // save keystrokes
 }
 
-res.setHeader('Access-Control-Allow-Origin', '*');
-
 /**
  * Shape object, parent
  */
